afe395af03702bd84d46db3c3216b16f1826847a
Brian Cerney <brianecerney@gmail.com>
2020-03-28 21:23:07 -0400

    Updates after first run troubleshooting.

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install software-properties-common
+sudo apt install \
+  git \
+  software-properties-common \
+  curl
+
+git config --global user.email "brianecerney@gmail.com"
+git config --global user.name "Brian Cerney"
+
+# Install Ansible
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
 
 # command to run playbook
+ansible-playbook -v ubuntu-desktop.yml --ask-become-pass
