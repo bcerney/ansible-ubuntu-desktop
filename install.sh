c9baace11e1b0376cba665a6f6fdf5cadd527561
Brian Cerney <brianecerney@gmail.com>
2020-03-29 12:36:47 -0400

    Updates to anki role.

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install \
-  git \
-  software-properties-common \
-  curl
+# sudo apt install \
+#   git \
+#   software-properties-common \
+#   curl
 
+# TODO: needed or moved to git role?
 git config --global user.email "brianecerney@gmail.com"
 git config --global user.name "Brian Cerney"
 
 # Install Ansible
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible
-
-# command to run playbook
+# Run playbook
 ansible-playbook -v ubuntu-desktop.yml --ask-become-pass
